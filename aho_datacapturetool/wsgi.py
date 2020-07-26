af26498a25a5166741f3f5c77e99c04cef5855c6
Stephen <smburu@uonbi.ac.ke>
2020-07-26 11:12:07 +0300

    Removed aho-app directory and modified wsgi and manage.py files

--- aho_datacapturetool/wsgi.py
+++ aho_datacapturetool/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for translate_project project.
-
+WSGI config for aho_datacapturetooldeveloped for WHO's AFRO iAHO.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aho_datacapturetool.settings')
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aho_datacapturetool.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aho_datacapturetool.settings')
 
 application = get_wsgi_application()
