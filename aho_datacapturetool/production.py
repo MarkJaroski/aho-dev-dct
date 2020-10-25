916291d3efc2c984c6dbc3cb72f68edb4c28646f
Stephen <smburu@uonbi.ac.ke>
2020-10-25 22:14:18 +0300

    Serge request for i18n and L10n support by translating DCT interface into 3 languages: English, French and Portuguese

--- aho_datacapturetool/production.py
+++ aho_datacapturetool/production.py
@@ -1,9 +1,9 @@
 from .settings import *
 
-DEBUG = False
+DEBUG = True
 # Configure default domain name
 ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.azurewebsites.net',
-'af-aho-datacapturetool.azurewebsites.net/fr'] if 'WEBSITE_SITE_NAME' in os.environ else []
+'localhost'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
 # WhiteNoise configuration
 MIDDLEWARE = [
@@ -11,6 +11,7 @@ MIDDLEWARE = [
 # Add whitenoise middleware after the security middleware
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware', # added to support language
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -20,9 +21,6 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale/'), # for UI language translations
-)
 
 # Configure Postgres database
 DATABASES = {
